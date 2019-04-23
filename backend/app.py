02d8183f8ecebb28db38893cf88e2e5ea92eac75
JemimaSadhika <43720203+JemimaSadhika@users.noreply.github.com>
2019-04-24 01:36:42 +0530

    Updated to use scrapped data

--- backend/app.py
+++ backend/app.py
@@ -3,40 +3,37 @@ import requests
 
 app = Flask(__name__)
 
-#routing to call  the specific url with integer id like "http://localhost:8006/api/pokemon/25"
-@app.route('/api/pokemon/<id>')
-def index(id):
- if ((int(id) >= 1) and( int(id)<= 151)):
-  
-     #initialising empty dict to store values of pokemon
-     data={}
-     data['pokemon']={}
-     
-     #storing the value of key 'id' in dict by extracting from route requested
-     data['pokemon']['id']= int(id)
-     
-     #calling RESTFUL API by dynamically generating URL and converting the data into json format for easy access
-     response = requests.get('https://pokeapi.co/api/v2/pokemon/{0}'.format(id)).json()
-     
-     #retrieving required values from json data stored in 'response' variable to store into pokemon dict
-     data['pokemon']['name']=response['species']['name']
-     data['pokemon']['sprite'] = response['sprites']['front_default']
-     
-     #json.dumps converts dict into string format and returns
-     return json.dumps(data)
- else:
+#routing to call  the specific url is http://localhost:8006/api/pokemon/id where id range 1 to 151
+@app.route('/api/pokemon/<int:id>')
+def specificIdData(id):
+    #list to hold retrieved file data
+    loadedFileData = []
+
+    #checking and retrieves data for id in between 1 and 151
+    if (id >= 1) and  (id<=151):
+    	#dict to hold json data
+	    data = {}
+		#opening and retrieving all data in the file
+	    with open('pokemondetails.json') as fileData: 
+		    loadedFileData = json.load(fileData)
+			#extracting specific id information from the list
+		    data['pokemon'] = loadedFileData[id-1] 
+		    #converts the dictionary into string format while returning data as dict cannot be directly displayed
+		    return json.dumps(data)
+
+    #when id is not in specific range error is displayed with 404 status code		   
+    else:
       return '<h1>The requested id is not valid. Please enter an id in between 1 and 151</h1>'  ,404
       
-#To handle the case when id requested is not int i.e "api/pokenmon/unknown"
-@app.errorhandler(ValueError)
-def not_integer(error=None):
- return '<h1>The id provided must be of type int</h1>',500
 
-# Error handling message when any other url with localhost:8006 is used
+
+# Error handling message when any other url with localhost:8006 is used or id is not of type integer
 @app.errorhandler(404)
 def file_not_found(error= None):
-    return '<h1>The entered URL is not valid. Please enter a valid one.</h1>',404      
+   return '<h1>The entered URL is not valid. Please enter a valid one.</h1>',404      
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8006, debug=True)
+
+    
